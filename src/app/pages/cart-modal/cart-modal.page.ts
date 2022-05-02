36dd8616fd347e9b46b46d8b8758b5ef756e8a97
mayneva <maynevaanna@gmail.com>
2022-05-02 12:14:04 +0300

    Creating our Cart Modal Page

--- src/app/pages/cart-modal/cart-modal.page.ts
+++ src/app/pages/cart-modal/cart-modal.page.ts
@@ -1,4 +1,6 @@
+import { Product, CartService } from './../../services/cart.service';
 import { Component, OnInit } from '@angular/core';
+import { ModalController, AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-cart-modal',
@@ -7,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartModalPage implements OnInit {
 
-  constructor() { }
+  cart: Product[] = [];
+
+  constructor(private cartService: CartService, private modalCtrl: ModalController, private alertCtrl: AlertController) { }
 
   ngOnInit() {
+    this.cart = this.cartService.getCart();
   }
 
+  decreaseCartItem(product) {
+    this.cartService.decreaseProduct(product);
+  }
+
+  increaseCartItem(product) {
+    this.cartService.addProduct(product);
+  }
+
+  removeCartItem(product) {
+    this.cartService.removeProduct(product);
+  }
+
+  getTotal() {
+    return this.cart.reduce((i, j) => i + j.price * j.amount, 0);
+  }
+
+  close() {
+    this.modalCtrl.dismiss();
+  }
+
+  async checkout() {
+    // Perfom PayPal or Stripe checkout process
+
+    let alert = await this.alertCtrl.create({
+      header: 'Thanks for your Order!',
+      message: 'We will deliver your flowers as soon as possible',
+      buttons: ['OK']
+    });
+    alert.present().then(() => {
+      this.modalCtrl.dismiss();
+    });
+  }
 }
